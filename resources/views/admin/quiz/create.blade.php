2236863a709931aaaece9269f4a63a69e8edd8a1
lalawiem <ko905096@gmail.com>
2022-10-29 11:06:01 +0300

    SON

--- resources/views/admin/quiz/create.blade.php
+++ resources/views/admin/quiz/create.blade.php
@@ -1,10 +1,13 @@
 <x-app-layout>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
 
-    <x-slot name="header">Quiz  Oluştur </x-slot>
+    <x-slot name="header">Quiz Oluştur </x-slot>
 
- 
+
 
 
     <div class="card">
@@ -21,31 +24,36 @@
                     <textarea name="description" class="form-control" rows=4">{{ old('description') }}</textarea>
                 </div>
 
-                <div class="form-group"> 
-                <input id="isFinished" @if(old('finished_at')) checked @endif type="checkbox">
+                <div class="form-group">
+                    <input id="isFinished" @if(old('finished_at')) checked @endif type="checkbox">
                     <label>Bitiş Tarihi Olacak mı? </label>
                 </div>
-                
-                <div id="finishedInput" @if(old('finished_at')) style="display:none" @endif class="form-group"> 
-                    <label>Bitiş Tarihi  </label>
-                    <input type="datetime-local" name="finished_at" value="{{ old('finished_at') }}" class="form-control">
+
+                <div id="finishedInput" @if(old('finished_at')) style="display:none" @endif class="form-group">
+                    <label>Bitiş Tarihi </label>
+                    <input type="datetime-local" name="finished_at" value="{{ old('finished_at') }}"
+                        class="form-control">
                 </div>
 
-                <div class="form-group"> 
-               <button type="submit" class="btn btn-success btn-sm btn-block">Quiz Oluştur</button>
-               </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-success btn-sm btn-block">Quiz Oluştur</button>
+                </div>
             </form>
-        </div> 
-    </div> 
-    <x-slot name="js">  
+        </div>
+    </div>
+    <x-slot name="js">
         <script>
-            $('#isFinished').change(function(){
-                if($('#isFinished').is(':checked')){
-                    $('#finishedInput').show();
-                }else {  
-                    $('#finishedInput').hide();
-                }
-            })
-            </script>
+        $('#isFinished').change(function() {
+            if ($('#isFinished').is(':checked')) {
+                $('#finishedInput').show();
+            } else {
+                $('#finishedInput').hide();
+            }
+        })
+        </script>
     </x-slot>
+
+
+
+
 </x-app-layout>
--- resources/views/admin/quiz/list.blade.php
+++ resources/views/admin/quiz/list.blade.php
@@ -1,40 +1,43 @@
-
 <x-app-layout>
     <x-slot name="header"> Quizler</x-slot>
     <link rel="stylesheet" href="app.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
 
-    
+
     <div class="card">
         <div class="card-body">
-              <h5 class="card-title">
-              <a href="{{route('quizzes.create')}}" class="btn btn-sm btn-primary"><i class="fa fa-plus"></i>Quiz Oluştur</a>
-              </h5>
-              <table class="table table-bordered">
-  <thead>
-    <tr>
-      <th scope="col">Quiz</th>
-      <th scope="col">Durum</th>
-      <th scope="col">Bitiş Tarihi</th>
-      <th scope="col">İşlemler</th>
-    </tr>
-  </thead>
-  <tbody>
-    @foreach($quizzes as $quiz)
-    <tr>
-      <th> {{ $quiz->title }}</th>
-      <td>{{ $quiz->status }}</td>
-      <td>{{ $quiz->finished_at }}</td>
-      <td>
-        <a href="#" class="btn btn-sm btn-primary"><i class="fa fa-edit"></i></a>
-        <a href="#" class="btn btn-sm btn-danger"><i class="fa fa-times"></i></a>
-        </td>
-    </tr>
-    @endforeach
-  </tbody>
-</table>
-{{$quizzes->links()}}
-              </div>
-              </div>
+            <h5 class="card-title">
+                <a href="{{route('quizzes.create')}}" class="btn btn-sm btn-primary"><i class="fa fa-plus"></i>Quiz
+                    Oluştur</a>
+            </h5>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">Quiz</th>
+                        <th scope="col">Durum</th>
+                        <th scope="col">Bitiş Tarihi</th>
+                        <th scope="col">İşlemler</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($quizzes as $quiz)
+                    <tr>
+                        <th> {{ $quiz->title }}</th>
+                        <td>{{ $quiz->status }}</td>
+                        <td>{{ $quiz->finished_at }}</td>
+                        <td>
+                            <a href="#" class="btn btn-sm btn-primary"><i class="fa fa-edit"></i></a>
+                            <a href="#" class="btn btn-sm btn-danger"><i class="fa fa-times"></i></a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            {{$quizzes->links()}}
+        </div>
+    </div>
 </x-app-layout>
